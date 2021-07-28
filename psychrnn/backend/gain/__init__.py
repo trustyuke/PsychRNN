b602c380c52f0cd75e201c1fb180fab9c86c4d62
Gary Kane <gakane@bu.edu>
2021-07-28 09:40:44 -0400

    adding basic network, loss functions, connectivity

---
+++ psychrnn/backend/gain/__init__.py
@@ -0,0 +1,4 @@
+from psychrnn.backend.gain.basic2 import Basic2
+from psychrnn.backend.gain.gain import AdaptiveGain
+from psychrnn.backend.gain.loss import rt_mask_mse_06, rt_mask_mse_07, rt_mask_mse_08
+from psychrnn.backend.gain.connectivity import multi_layer_connectivity
