80e37fffbe2e8c1e7114a8de3081f84e6d41ab07
tianw6 <protoss0318@gmail.com>
2021-10-27 14:07:37 -0400

    explore why KiNeT of RNN is opposite of real data

--- firingRateAnalysis/realKiNeT.m
+++ firingRateAnalysis/realKiNeT.m
@@ -1,3 +1,4 @@
+% the kinet analysis of real pmd data
 clear all; close all; clc
 % for linux work station 
 temp = load("/home/tianwang/code/behaviorRNN/PsychRNNArchive/stateActivity/spikes.mat").trials;
@@ -7,6 +8,54 @@ RT = checker.RT;
 RTR = round(RT, -1);
 
 [a, b, c] = size(temp);
+
+
+%% 
+
+% rt = 300:100:800;
+% right = checker.choice == 2;
+% left = checker.choice == 1;
+% 
+% leftTraj = NaN(a,length(rt)-1,b);
+% rightTraj = NaN(a,length(rt)-1,b);
+% leftRT = NaN(length(rt)-1,1);
+% rightRT = NaN(length(rt)-1,1);
+% 
+% for ii  = 1 : length(rt) - 1
+%     selectedTrials = (rt(ii) < RTR & RTR < rt(ii + 1));
+% 
+%     leftSelect = selectedTrials & left;
+%     rightSelect = selectedTrials & right;
+%     leftTrajAve = mean(temp(:,:,leftSelect), 3);
+%     rightTrajAve = mean(temp(:,:,rightSelect), 3);
+%     
+%     leftTraj(:,ii,:) = leftTrajAve;
+%     rightTraj(:,ii,:) = rightTrajAve;
+%     
+%     % left and right average RT of each RT bin, should add 50 to be the
+%     % real time from start
+%     leftAveRT = round(mean(RTR(leftSelect))) + 60;
+%     rightAveRT = round(mean(RTR(rightSelect))) + 60;
+%     
+%     leftRT(ii) = leftAveRT;
+%     rightRT(ii) = rightAveRT;
+%     
+% end
+% 
+% 
+% [a, b, c] = size(leftTraj);
+% 
+% test = reshape(leftTraj, [a, b*c])';
+% 
+% [coeff, score, latent] = pca(test);
+% orthF = [];
+% for thi = 1 : c
+%     orthF(:,:,thi) = (score( (1:b) + (thi-1)*b, :))';
+% end
+% 
+% 
+% addpath("./KiNeT-master");
+% KiNeT(orthF(1:10, :,:),1);
 
 %%
 test = reshape(temp, [a, b*c])';
@@ -49,7 +98,9 @@ for ii  = 1 : length(rt) - 1
     
 end
 
+
+
 %% KiNeT analysis
 
 addpath("./KiNeT-master");
-KiNeT(rightTraj(:,:,:),1);
+KiNeT(leftTraj(:,:,:),1);
